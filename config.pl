bcd15809e2a9bce9bea302f14d53fbd22ff70070
masartz <masartz@gmail.com>
2011-05-12 20:26:10 +0900

    add config.pl

---
+++ config.pl
@@ -0,0 +1,9 @@
+=format
+    {
+        feed_url => 'http://sample.com/timeline?format=rss',
+    }
+=cut
+
+{
+    feed_url => 'http://coderepos.org/share/timeline?ticket=on&changeset=on&milestone=on&wiki=on&max=50&daysback=90&format=rss',
+}
